093c4534fe0ab63204f1e78cf034c057cfadb0df
vinodh1988 <k.c.vinodh@hotmail.com>
2022-08-17 12:12:19 +0530

    conversion-commit

--- lists.R
+++ lists.R
@@ -2,6 +2,7 @@ list_items <- list( c(1,23,53),c('apple','boy','cat'),"India")
 
 names(list_items) <- c("Numbers","Strings","Just")
 
+
 print(list_items)
 
 print(list_items[1])
@@ -12,3 +13,10 @@ clist2<- c("Ram",25,"Rahul")
 
 print(class(clist))
 print(class(clist2))
+
+print(typeof(clist))
+print(typeof(clist2))
+print(typeof(list_items[[1]]))
+print(typeof(list_items[[3]]))
+
+print(class(list_items[[1]]))
---
+++ scalarconversion.R
@@ -0,0 +1,13 @@
+a="20" 
+
+print(class(a))
+b<- as.numeric(a)
+
+print(class(a))
+print(class(b))
+
+logic1=as.logical(30)
+logic2=as.logical(0)
+
+print(logic1)
+print(logic2)
