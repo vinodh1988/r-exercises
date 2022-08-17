7dccfb6023d7c766da3053f511e6f66701f8fff3
vinodh1988 <k.c.vinodh@hotmail.com>
2022-08-17 12:29:39 +0530

    matrix

--- matrix.R
+++ matrix.R
@@ -1,3 +1,9 @@
-mymatrix=matrix(c(1,35,35,43),nrow=2,ncol=2)
+mymatrix=matrix(c(1,35,39,43),nrow=2,ncol=2)
+
+print(mymatrix)
+
+print(mymatrix[1,2])
+
+mymatrix=matrix(c(1,35,39,43),nrow=2,ncol=2,byrow=TRUE)
 
 print(mymatrix)
