15341ef150f2cac25f6c2f40a4ca117dde5e181b
vinodh1988 <k.c.vinodh@hotmail.com>
2022-08-17 11:56:50 +0530

    names-commit

--- lists.R
+++ lists.R
@@ -1,8 +1,11 @@
 list_items <- list( c(1,23,53),c('apple','boy','cat'),"India")
 
+names(list_items) <- c("Numbers","Strings","Just")
+
 print(list_items)
 
 print(list_items[1])
+print(list_items$Strings)
 
 clist<- c(1,2,3)
 clist2<- c("Ram",25,"Rahul")
