b4d3e528447e897a686b09aaabca07b910bdfe79
vinodh1988 <k.c.vinodh@hotmail.com>
2022-08-17 11:50:23 +0530

    list

--- lists.R
+++ lists.R
@@ -1,3 +1,11 @@
 list_items <- list( c(1,23,53),c('apple','boy','cat'),"India")
 
 print(list_items)
+
+print(list_items[1])
+
+clist<- c(1,2,3)
+clist2<- c("Ram",25,"Rahul")
+
+print(class(clist))
+print(class(clist2))
