1e752b6d07ee2b7bd440eaf404acc26573aee2de
vinodh1988 <k.c.vinodh@hotmail.com>
2022-08-17 11:34:17 +0530

    index-commit

--- vectorops.R
+++ vectorops.R
@@ -3,5 +3,18 @@ v2<-c(234,35,35,34,56)
 
 v1+v2 ->ops.sum
 
+v1-v2 ->ops.difference
+
 cat("v1+v2",ops.sum)
 cat("length of v1", length(v1))
+cat("v1-v2",ops.difference)
+
+for ( x in ops.sum){
+    cat("\n ",x)
+}
+
+cat("\nWay 2 ################")
+
+for ( index in 1:length(ops.sum) )
+   cat("\n ",index," -> ",ops.sum[index])
+
