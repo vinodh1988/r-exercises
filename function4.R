7c512131178def3c9c160178149bdd963673f154
vinodh1988 <k.c.vinodh@hotmail.com>
2022-08-18 10:09:01 +0530

    reduce-operation

--- function4.R
+++ function4.R
@@ -6,3 +6,9 @@ processed=Map(function(x) toupper(x),namelist)
 
 for(x in processed)
   cat(x," -> ")
+
+numericlist=c(1,35,34,5,6,899,2393,34,353,536,342,35,2564,3,3)
+
+
+result = Reduce(function(x,y) if(x>y) x else y,numericlist)
+cat('Result',result)
