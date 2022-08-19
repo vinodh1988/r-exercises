6a6f5f1dd343a04551dfee0bccdede18aa9f254a
vinodh1988 <k.c.vinodh@hotmail.com>
2022-08-19 11:09:01 +0530

    insert-prepared

--- insertrecords.R
+++ insertrecords.R
@@ -1,5 +1,8 @@
 con <- dbConnect(SQLite(),"sample.db")
 
+lim<- as.integer(readline(prompt="how many records you want to insert -> "))
+i<- 1
+while(i<=lim){
 x<-as.integer(readline(prompt="enter sno ->"))
 y<-readline(prompt="enter name ->")
 z<-readline(prompt="enter city ->")
@@ -7,5 +10,6 @@ z<-readline(prompt="enter city ->")
 result <-dbSendQuery(con,"insert into books(sno,name,type) values(?,?,?)")
 dbBind(result,list(x,y,z))
 print(result)
-
+i<-i+1
 dbClearResult(result)
+}
