f72b4425911031dc9571df95d43169a1dac3d57e
vinodh1988 <k.c.vinodh@hotmail.com>
2022-08-19 09:52:03 +0530

    sqlit

--- tables.R
+++ tables.R
@@ -2,3 +2,12 @@ library(RSQLite)
 library(DBI)
 
 con <- dbConnect(SQLite(),"sample.db")
+
+books <- data.frame(
+    sno = c(1,2,3,4,5),
+    name = c("C# Primer","Java Primer","SQL","MongoDB","AWS Reference Manual"),
+    type = c('Programming','Programming','Database','Database','Cloud')
+) 
+
+#dbCreateTable(con,"books",books)
+dbWriteTable(con,"books",books)
